bf39fb8c4f9b574309852df038f2b36ff2557e69
jbped <pedigojacob@gmail.com>
2021-07-07 20:01:36 -0600

    refactor delete function, changed app title references to Scratch Note in HTML files

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -2,28 +2,16 @@ const router = require("express").Router();
 const db = require("../../db/db.json");
 const { createNote, deleteNote, getNotes } = require("../../lib/notes");
 
-
+// =======================================
+//                ROUTES
+// =======================================
 router.get("/notes", (req,res) => {
     let notesArr = getNotes();
     let notes = notesArr;
-    console.log(notes)
     res.json(notes);
 });
 
 router.post("/notes", ({ body }, res) => {
-    let notesArr = getNotes();
-    unallowedIds =  notesArr.map(note => parseInt(note.id));
-    console.log(unallowedIds);
-    console.log(notesArr.length);
-    if (!unallowedIds.includes(notesArr.length)) {
-        console.log(true)
-        body.id = notesArr.length.toString()
-    } else {
-        body.id = (Math.max(...unallowedIds) + 1).toString();
-        console.log(body.id)
-    }
-    // console.log(unallowedIds);
-    // body.id = notesArr.length.toString();
     const note = createNote(body);
     res.json(note);
 });
@@ -35,5 +23,3 @@ router.delete("/notes/:id", (req, res) => {
 });
 
 module.exports = router;
-
-// 
