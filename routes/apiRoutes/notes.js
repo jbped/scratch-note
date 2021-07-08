f0fb9f6ceac9129e723f92ccd3b487549c7cc98e
jbped <pedigojacob@gmail.com>
2021-07-07 19:49:15 -0600

    Post note IDs are unique, needs refactor

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -4,22 +4,36 @@ const { createNote, deleteNote, getNotes } = require("../../lib/notes");
 
 
 router.get("/notes", (req,res) => {
-    let notes = getNotes();
+    let notesArr = getNotes();
+    let notes = notesArr;
     console.log(notes)
     res.json(notes);
 });
 
 router.post("/notes", ({ body }, res) => {
-    body.id = db.length.toString();
-    const note = createNote(body, db);
+    let notesArr = getNotes();
+    unallowedIds =  notesArr.map(note => parseInt(note.id));
+    console.log(unallowedIds);
+    console.log(notesArr.length);
+    if (!unallowedIds.includes(notesArr.length)) {
+        console.log(true)
+        body.id = notesArr.length.toString()
+    } else {
+        body.id = (Math.max(...unallowedIds) + 1).toString();
+        console.log(body.id)
+    }
+    // console.log(unallowedIds);
+    // body.id = notesArr.length.toString();
+    const note = createNote(body);
     res.json(note);
 });
 
 router.delete("/notes/:id", (req, res) => {
     let id = req.params.id
-    
     let noteArr = deleteNote(id);
     res.json(noteArr);
 });
 
 module.exports = router;
+
+// 
